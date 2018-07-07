7354f22220ee9b8a3043292ba5d69059bd7f6811
Ashwin Karthik Tamilselvan <at3103@columbia.edu>
2018-07-08 02:15:45 +0530

    cleanup

--- utils/setup.py
+++ utils/setup.py
@@ -1,9 +1,9 @@
 """
 This module contains all the constants used.
 """
-from config import SLACK_BOT_TOKEN, OWM_API_KEY
-from slackclient import SlackClient
 import pyowm
+from slackclient import SlackClient
+from config import SLACK_BOT_TOKEN, OWM_API_KEY
 from enum import Enum
 
 # starterbot's user ID in Slack: value is assigned after the bot starts up
@@ -24,17 +24,19 @@ NOT_IMPLEMENTED = "Sure...write some more code then I can do that!"
 GREETINGS = "Welcome <@{user_id}> to the channel <#{channel}>! Hope you have a good time here!"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 WEATHER_REGEX = "*weather*"
-WEATHER="weather"
 
 
 msg_to_resp = {"example":NOT_IMPLEMENTED,
                "greeting":GREETINGS,
-               "default":"Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND),
-               "weather":WEATHER}
+               "default":"Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)}
 
 city_name_userid = dict()
 
 class Labels(Enum):
+    """
+    Enum for the different type of commands for the slack bot.
+    """
+
     example = "example"
     greeting = "greeting"
     default = "default"
@@ -42,11 +44,3 @@ class Labels(Enum):
     weather_my_loc = "weather in my location"
     dnt_knw_loc = "My Apologies! I don't have the capability to guess your current location yet!\
      I'm still learning. Please tell me your city, I'll remember it the next time. Trust me!"
-
-
-
-# constants
-# RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-# EXAMPLE_COMMAND = "do"
-# GREETINGS = "Welcome"
-# MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
